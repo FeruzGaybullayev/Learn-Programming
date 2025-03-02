2585d74f8b78a6acf7adc35ed10b2cb0c4d3dbee
Feruz <gaybullayevferuzjon108@gmail.com>
2025-03-02 07:14:48 +0500

    Update factors.py

--- lesson-5/homework/factors.py
+++ lesson-5/homework/factors.py
@@ -1,3 +1,7 @@
+# Task 3
+# A factor of a positive integer n is any positive integer less than or equal to n that divides n with no remainder.
 
-
-user_input = input('Enter a positive integer: ')
+number = input('Enter a positive integer: ')
+for num in range(1, number+1):
+  if bunber % num == 0:
+    print(f'{num} is factor of {number}')
