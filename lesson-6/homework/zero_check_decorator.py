2c9ec9f6fc717e0709383463e80e3db23fe5177d
FeruzGaybullayev <gaybullayevferuzjon108@gmail.com>
2025-03-03 18:14:15 +0500

    lesson

--- lesson-6/homework/zero_check_decorator.py
+++ lesson-6/homework/zero_check_decorator.py
@@ -1,15 +1,15 @@
-# Zero Check Decorator 
+# Zero Check Decorator
 def check(func):
     def wrapper(a, b):
         try:
-            return func(a, b)
+            return a/b
         except ZeroDivisionError:
-            print('Detorminator can\'t be zero')
+            print("Detorminator can't be zero")
     return wrapper
 
 @check
 def div(a, b):
     return a/b
 
-print(div(5, 4))
-print(div(9, 0))
+print(div(6, 2))
+print(div(6, 0))
